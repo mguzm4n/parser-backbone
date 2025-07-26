569fbf3a52133b3eb5887996aa52494d841f55f5
Marcelo <marcelo.guzman.g@usach.cl>
2025-07-25 20:12:54 -0400

    add basic generation for expression types and visitor

---
+++ multichar-parser/.tools/generate-ast.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+
+
+def define_visitor(program: list[str], base_name, types: dict[str, list[str]]):
+  program.append("type Visitor interface {")
+  for (type, _) in types.items():
+    program.append(f"Visit{type}{base_name}(expr *{type}) interface{{}}")
+  program.append("}")
+
+
+def define_ast(out: Path, base_name: str, types: dict[str, list[str]]):
+  program = []
+  
+  program.append("package parser")
+  program.append('import ( "mguzm4n/multichar-parser/src/lexer" )')
+  
+  program.append(f"type {base_name} interface {{")
+  program.append("isExpr()")
+  program.append("Accept (v Visitor) interface{}")
+  program.append("}")
+  
+  for (type, members) in types.items(): 
+    
+    # define struct
+    program.append(f"type {type} struct {{")
+    program.append("\n".join(members))
+    program.append("}")
+    
+    # interface compliance
+    program.append(f"func ({type}) isExpr() {{ }}")
+    
+    # visitor pattern
+    program.append(f"func ({type[0].lower()} *{type}) Accept(visitor Visitor) interface{{}} {{")
+    program.append(f"return visitor.Visit{type}{base_name}({type[0].lower()})")
+    program.append("}")
+    
+    # constructor and fields
+    cons_members = ", ".join(members)
+    inst_members = ", ".join(
+      list(map(lambda s: s.split(" ")[0], members))
+    )
+    program.append(f"func New{type}({cons_members}) *{type} {{")
+    program.append(f"return &{type}{{")
+    program.append(inst_members + ",")
+    program.append("}")
+    program.append("}")
+    
+  
+  define_visitor(program, base_name, types)
+  
+  with open(out, 'w') as f:
+    f.writelines(line + "\n" for line in program)
+    
+    
+def main(out: Path):
+  define_ast(
+    out,
+    "Expr", {
+      "Binary": ["Left Expr", "Operator lexer.Token", "Right Expr"],
+      "Grouping": ["Expression Expr"],
+      "Literal": ["Value any"],
+      "Unary": ["Operator lexer.Token", "Right Expr"]
+    }
+  )
+
+
+if __name__ == "__main__":
+  this_dir = Path(__file__).parent
+  target_dir = this_dir.parent / "src" / "parser"
+  if not target_dir.exists:
+    raise Exception("directory not found")
+    
+  filename = "exprdefs.go"
+  main(target_dir / filename)
