81ebf62c869d0403a3859762b9dcde2f70ca38ca
Marcelo <marcelo.guzman.g@usach.cl>
2025-07-26 15:15:47 -0400

    setup main.go again

--- multichar-parser/src/main.go
+++ multichar-parser/src/main.go
@@ -1,9 +1,29 @@
 package main
 
-import "mguzm4n/multichar-parser/src/lexer"
+import (
+	"fmt"
+	"mguzm4n/multichar-parser/src/lexer"
+	"mguzm4n/multichar-parser/src/parser"
+)
 
 func main() {
-	lex := lexer.New(`"hey 12.5"1200 + 23.1001`)
+	lex := lexer.New(`("what" + 1) "espacio"`)
 	lex.Scan()
 	lex.DbgPrintTokens()
+
+	// expr := parser.NewBinary(
+	// 	parser.NewUnary(
+	// 		lexer.Token{Type: lexer.Minus, Lexeme: "-", Literal: nil},
+	// 		parser.NewLiteral(11),
+	// 	),
+	// 	lexer.Token{Type: lexer.Star, Lexeme: "*", Literal: nil},
+	// 	parser.NewGrouping(parser.NewLiteral(22.5)),
+	// )
+
+	rdParser := parser.NewParser(lex.Tokens())
+	expr := rdParser.Parse()
+
+	visitor := parser.StringVisitor{}
+	fmt.Println(visitor.Print(expr))
+
 }
